411df820729033bcd2882405168bcc14ba9f611f
Brian Zilka <Bzilks@gmail.com>
2019-05-09 08:56:54 -0500

    updated db.sql

--- pet_hotel.sql
+++ pet_hotel.sql
@@ -3,7 +3,8 @@ CREATE TABLE "pets"
     "id" SERIAL PRIMARY KEY,
     "pet_name" VARCHAR(150) NOT NULL,
     "pet_color" VARCHAR(50),
-    "pet_breed" VARCHAR(50)
+    "pet_breed" VARCHAR(50),
+    "checked_in" VARCHAR(25)
 );
 
 CREATE TABLE "owners"
