9bf925e365858162b92b8f1db3574cf2025db7c3
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-05-09 13:36:51 -0500

    delete owner success test on postman

--- api/objects/Owner.php
+++ api/objects/Owner.php
@@ -56,4 +56,26 @@ class Owner
 
       return false;
    }
+
+   function delete() 
+   {
+      $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+      
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->id=htmlspecialchars(strip_tags($this->id));
+ 
+    // bind id of record to delete
+    $stmt->bindParam(1, $this->id);
+ 
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+   
+   }
 }
