5a24a708cfae263a269ae7419d6625e72c289a6b
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-05-09 10:06:34 -0500

    pets/create

--- api/pets/create.php
+++ api/pets/create.php
@@ -10,48 +10,48 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 include_once '../config/database.php';
 
 // instantiate product object
-include_once '../objects/product.php';
+include_once '../objects/pets.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-$product = new Product($db);
+$pet = new Pets($db);
 
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
 
 // make sure data is not empty
 if(
-    !empty($data->title) &&
-    !empty($data->publisher) &&
-    !empty($data->releaseYear) &&
-    !empty($data->rating)
+    !empty($data->pet_name) &&
+    !empty($data->pet_color) &&
+    !empty($data->pet_breed) &&
+    !empty($data->check_in)
 ){
 
     // set product property values
-    $product->title = $data->title;
-    $product->publisher = $data->publisher;
-    $product->releaseYear = $data->releaseYear;
-    $product->rating = $data->rating;
+    $pet->pet_name = $data->pet_name;
+    $pet->pet_color = $data->pet_color;
+    $pet->pet_breed = $data->pet_breed;
+    $pet->check_in = $data->check_in;
 
-    // create the product
-    if($product->create()){
+    // create the pets
+    if($pet->create()){
 
         // set response code - 201 created
         http_response_code(201);
 
         // tell the user
-        echo json_encode(array("message" => "Product was created."));
+        echo json_encode(array("message" => "Pet was created."));
     }
 
-    // if unable to create the product, tell the user
+    // if unable to create the pet, tell the user
     else{
 
         // set response code - 503 service unavailable
         http_response_code(503);
 
         // tell the user
-        echo json_encode(array("message" => "Unable to create product."));
+        echo json_encode(array("message" => "Unable to create pet."));
     }
 }
 
@@ -62,5 +62,5 @@ else{
     http_response_code(400);
 
     // tell the user
-    echo json_encode(array("message" => "Unable to create product. Data is incomplete."));
+    echo json_encode(array("message" => "Unable to create pet. Data is incomplete."));
 }
