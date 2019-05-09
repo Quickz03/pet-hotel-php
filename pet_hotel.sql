f93475a87638dbaf8f7b6aab825b86a081c6d29f
Will Howes <willmhowes@gmail.com>
2019-05-09 13:40:33 -0500

    update database

--- pet_hotel.sql
+++ pet_hotel.sql
@@ -1,35 +1,32 @@
 -- CREATE DATABASE "pet_hotel"
 
-CREATE TABLE "pets"
-(
+CREATE TABLE "owners" (
+    "id" SERIAL PRIMARY KEY,
+    "owner_name" VARCHAR(80) NOT NULL,
+);
+
+CREATE TABLE "pets" (
     "id" SERIAL PRIMARY KEY,
     "pet_name" VARCHAR(150) NOT NULL,
     "pet_color" VARCHAR(50),
     "pet_breed" VARCHAR(50),
     "checked_in" BOOLEAN
+    "owner_id" INT REFERENCES "owners"
 );
 
-CREATE TABLE "owners"
-(
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(80) NOT NULL,
-    "pet_id" INT REFERENCES "pets"
-);
-
-INSERT INTO "pets" ("pet_name", "pet_color", "pet_breed", "checked_in")
+INSERT INTO "owners" ("name")
 VALUES
-('Thompson', 'White w/ Black Spots', 'Terrier-Poodle Mix', false),
-('Big Mac', 'Black w/ Grey Stripes', 'Cat', true),
-('Gabe', 'White', 'Dog', true),
-('Lloyd', 'Orange', 'Cat', false),
-('Tuna', 'Grey', 'Cat', false),
-('Mackerel', 'Black', 'Cat', true);
+('Will'),
+('Joe'),
+('Brian'),
+('Ben'),
+('Rowan');
 
-INSERT INTO "owners" ("name", "pet_id")
+INSERT INTO "pets" ("pet_name", "pet_color", "pet_breed", "checked_in", "owner_id")
 VALUES
-('Will', '1'),
-('Joe', '2'),
-('Brian', '3'),
-('Ben', '4'),
-('Rowan', '5'),
-('Rowan', '6');
+('Thompson', 'White w/ Black Spots', 'Terrier-Poodle Mix', false, 1),
+('Big Mac', 'Black w/ Grey Stripes', 'Cat', true, 2),
+('Gabe', 'White', 'Dog', true, 3),
+('Lloyd', 'Orange', 'Cat', false, 4),
+('Tuna', 'Grey', 'Cat', false, 5),
+('Mackerel', 'Black', 'Cat', true, 5);
